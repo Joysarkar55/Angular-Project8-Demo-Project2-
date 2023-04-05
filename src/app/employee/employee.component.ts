dafa0481edc457016bda5bd08f36792346c2bc9b
JS952001 <sarkarpritam871@gmail.com>
2023-04-05 13:13:02 +0530

    first commit

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,28 @@
+/*import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent {
+
+}*/
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  bioSection = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    age: new FormControl('')
+  });
+constructor() { }
+ngOnInit() {
+  }
+}
+
